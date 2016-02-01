3910af5263b675ddad99af129b6391ee9413a88b
Karim Alibhai <k4rim.sa@gmail.com>
2016-02-01 13:21:57 -0500

    add java class creator

---
+++ bin/jcreate.js
@@ -0,0 +1,47 @@
+/**
+ * jcreate.js - jtools
+ * Copyright (C) 2016 VVBKA.
+ */
+
+// Author: Tony de Peltrie
+// Student number: 19850712
+// Course: ITI 1121-G
+// Assignment: 10
+// Question: 41
+
+'use strict';
+
+var fs = require('fs');
+
+module.exports = [{
+    main: ['m', 'Set main class to FILE.', 'file'],
+    project: ['p', 'Set project name.', 'string']
+}, function (args, opts, config) {
+    // treat the main class as a regular class during
+    // creation
+    if (opts.main) {
+        args.push(opts.main);
+    }
+
+    // create each file
+    args.forEach(function (file) {
+        if (fs.existsSync(file + '.java')) {
+            console.log('Skipping: %s.java (already exists)', file);
+            return;
+        }
+        
+        let data = config.compile({
+            classname: file,
+            filename: file + '.java',
+            project_name: opts.project || config.project_name
+        });
+
+        fs.writeFile('./' + file + '.java', data[(opts.main === file ? 'main_' : '') + 'file_contents'] + '\n', function (err) {
+            if (err) {
+                console.error('Error: failed to create: %s.java', file);
+                console.error(err);
+                process.exit(-1);
+            }
+        });
+    });
+}];
