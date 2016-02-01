e62a8e53541bdad47a0027d8e5da4c5599c08032
Karim Alibhai <k4rim.sa@gmail.com>
2016-02-01 13:21:52 -0500

    add global config editor

---
+++ bin/jconfig.js
@@ -0,0 +1,32 @@
+/**
+ * jcreate.js - jtools
+ * Copyright (C) 2016 VVBKA.
+ */
+
+// Author: Tony de Peltrie
+// Student number: 19850712
+// Course: ITI 1121-G
+// Assignment: 10
+// Question: 41
+
+'use strict';
+
+var fs = require('fs'),
+    path = require('path');
+
+module.exports = [{
+    list: ['l', 'List the config.'],
+    compile: ['c', 'Show the compiled config.'],
+    delete: ['d', 'Delete specified key.', 'string']
+}, function (args, opts, config) {
+    if (opts.list || opts.compile) {
+        console.log(JSON.stringify(opts.compile ? config.compile() : config, null, 2));
+    } else if (opts.delete) {
+        delete config[opts.delete];
+        fs.writeFile(path.resolve(__dirname, '..', '.jtoolsrc'), JSON.stringify(config, null, 2));
+    } else {
+        var key = args[0], value = args.slice(1).join(" ");
+        config[key] = value;
+        fs.writeFile(path.resolve(__dirname, '..', '.jtoolsrc'), JSON.stringify(config, null, 2));
+    }
+}];
